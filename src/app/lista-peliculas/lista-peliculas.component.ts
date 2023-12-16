4ad026d5e51afd71453c2fcecb6c8ae60f8d9aba
Roosevelt08 <alexraod21@gmail.com>
2023-12-16 02:35:40 -0500

    segundo  avance

--- src/app/lista-peliculas/lista-peliculas.component.ts
+++ src/app/lista-peliculas/lista-peliculas.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -8,28 +8,37 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ListaPeliculasComponent implements OnInit {
 
-  @Input() pelicula: any; // Asegúrate de tener esta línea para definir la propiedad de entrada pelicula
   peliculas: any[] = [];
+  generos: string[] = [];
+  generoSeleccionado: string = 'Todos';
+  nombreFiltro: string = '';
+  descripcionFiltro: string = '';
 
   constructor(private http: HttpClient) { }
 
-  listaPeliculas = [
-    { titulo: 'Pelicula 1', director: 'Director 1' },
-    { titulo: 'Pelicula 2', director: 'Director 2' },
-    // ... más películas
-  ];
-
   ngOnInit() {
     this.http.get('assets/peliculas.json').subscribe((data: any) => {
-      this.peliculas = data; // Asignar los datos recuperados a this.peliculas
+      this.peliculas = data.movies;
+      this.generos = data.genre; // Asignar los datos recuperados a this.peliculas
     });
   }
 
-  buscarPeliculas(event: Event) {
-    const valor = (event.target as HTMLInputElement)?.value;
-    if (valor !== null && valor !== undefined) {
-      // Lógica para buscar películas con el valor proporcionado
-    }
+   buscarPeliculas(event: Event) {
+     const valor = (event.target as HTMLInputElement)?.value;
+     if (valor !== null && valor !== undefined) {
+       // Lógica para buscar películas con el valor proporcionado
+     }
+   }
+
+   aplicarFiltros() {
+    this.peliculas = this.peliculas.filter(pelicula => {
+      const cumpleGenero = this.generoSeleccionado === 'Todos' || pelicula.genre === this.generoSeleccionado;
+      const cumpleNombre = pelicula.title.toLowerCase().includes(this.nombreFiltro.toLowerCase());
+      const cumpleDescripcion = pelicula.description.toLowerCase().includes(this.descripcionFiltro.toLowerCase());
+      return cumpleGenero && cumpleNombre && cumpleDescripcion;
+    });
   }
+  
+  
 }
 
